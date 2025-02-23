bf19eada41b402fb33e6513750b25b98a47bdc31
Alok Singh <alokmilenium@gmail.com>
2025-02-23 08:24:04 +0530

    fix: suppress pydantic user warnings in initialization
    Signed-off-by: Alok Singh <alokmilenium@gmail.com>

--- sgpt/__init__.py
+++ sgpt/__init__.py
@@ -1,2 +1,6 @@
 from .app import main as main
 from .app import entry_point as cli  # noqa: F401
+
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning,
+module="pydantic._internal._config")
